66a625eb66dd9f8165ee7ff263d0ca737d6f2dce
Jackson <sealj553@gmail.com>
2018-06-28 11:57:50 -0400

    initial commit

---
+++ spi.c
@@ -0,0 +1,43 @@
+#include "spi.h"
+
+#include <mpsse.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SPI_DEV SPI0
+#define REG_SZ 2
+
+static mpsse_context *flash = NULL;
+uint8_t dataRead[2];
+uint8_t dataWrite[2];
+
+int spi_init(){
+    return (flash = MPSSE(SPI_DEV, TWELVE_MHZ, MSB)) != NULL && flash->open;
+}
+
+const char* spi_get_error(){
+    return ErrorString(flash);
+}
+
+void spi_rw(){
+    Start(flash);
+    FastWrite(flash, (char*)dataWrite, REG_SZ);
+    FastRead(flash, (char*)dataRead, REG_SZ);
+    Stop(flash);
+}
+
+uint8_t ReadRegister(uint8_t reg){
+    dataWrite[0] = reg & 0x7F;
+    dataWrite[1] = 0x00;
+
+    spi_rw();
+
+    return dataRead[1];
+}
+
+void WriteRegister(uint8_t reg, uint8_t value){
+    dataWrite[0] = reg | 0x80;
+    dataWrite[1] = value;
+
+    spi_rw();
+}
---
+++ spi.h
@@ -0,0 +1,13 @@
+#ifndef SPI_H
+#define SPI_H
+
+#include <stdint.h>
+
+int spi_init();
+void spi_rw();
+const char* spi_get_error();
+
+uint8_t ReadRegister(uint8_t reg);
+void WriteRegister(uint8_t reg, uint8_t value);
+
+#endif
